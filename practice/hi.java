a4ec1a97fdd694b95e9b4eb0176e6c4b508a0dff
Tan Wei Junn <weijunntan@gmail.com>
2021-10-05 21:32:09 +0800

    first change

--- practice/hi.java
+++ practice/hi.java
@@ -3,6 +3,7 @@ package practice;
 public class hi {
     public static void main(String[] args) {
         System.out.println("Testing again");
+        System.out.println("Try again");
     }
     
 }
